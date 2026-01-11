9cb1d997a4d28eb40e927561d188d682f01b9229
theplatypus <nicolas.bloyet@gmail.com>
2026-01-12 00:06:27 +0100

    interface with networkx

---
+++ nx_scott/cgraph.py
@@ -0,0 +1,9 @@
+class CGraph:
+    def __init__(self, trace: str):
+        self.trace = trace
+
+    def __str__(self):
+        return self.trace
+
+    def __repr__(self):
+        return self.trace
