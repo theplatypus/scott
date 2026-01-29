ce3069f88d822d35082b3882b5dd791ea05eb78f
theplatypus <nicolas.bloyet@gmail.com>
2026-01-30 00:57:28 +0100

    Enabled PyO3 extension build

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,11 @@ pub mod error;
 pub mod graph;
 pub mod parse;
 pub mod tree;
+mod py;
+
+use pyo3::prelude::*;
+
+#[pymodule]
+fn _scott(py: Python<'_>, m: &Bound<'_, PyModule>) -> PyResult<()> {
+	py::init_module(py, m)
+}
